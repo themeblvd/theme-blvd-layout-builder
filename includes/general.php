880ff3d259a3150ee26ae564f142c9d111a76825
Jason Bobich <jasonbobich@gmail.com>
2014-12-08 12:47:03 -0900

    Fixed output of warning HTML

--- includes/general.php
+++ includes/general.php
@@ -50,7 +50,7 @@ function themeblvd_builder_layout( $context ) {
 
 	if ( ! $section_data ) {
 		echo '<section class="element-section">';
-		printf('<div class="alert alert-warning">%s</div>', __('The template has not been configured yet.', 'theme-blvd-layout-builder'));
+		printf('<div class="element"><div class="alert alert-warning">%s</div></div>', __('The template has not been configured yet.', 'theme-blvd-layout-builder'));
 		echo '</section>';
 		return;
 	}
@@ -126,7 +126,7 @@ function themeblvd_builder_layout( $context ) {
 	} else {
 
 		echo '<section class="element-section">';
-		printf('<div class="alert alert-warning">%s</div>', __('No element data could be found for this custom layout.', 'theme-blvd-layout-builder'));
+		printf('<div class="element"><div class="alert alert-warning">%s</div></div>', __('No element data could be found for this custom layout.', 'theme-blvd-layout-builder'));
 		echo '</section>';
 
 	}
