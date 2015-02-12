1fa4abf5bbeb7a94b2d941d919bb31c3299b811a
Jason Bobich <jasonbobich@gmail.com>
2015-02-11 15:26:43 -0900

    Added options to Content element

--- includes/api/class-tb-builder-api.php
+++ includes/api/class-tb-builder-api.php
@@ -1023,6 +1023,20 @@ class Theme_Blvd_Builder_API {
 				'type'		=> 'checkbox',
 				'std'		=> '1'
 			),
+			'center' => array(
+				'id' 		=> 'center',
+				'name'		=> null,
+				'desc'		=> __( 'Center all text within content.', 'theme-blvd-layout-builder' ),
+				'type'		=> 'checkbox',
+				'std'		=> '0'
+			),
+			'max' => array(
+				'id' 		=> 'max',
+				'name'		=> __( 'Maximum Width', 'theme-blvd-layout-builder' ),
+				'desc'		=> __( 'If you\'d like to limit the width of the content unit, give it a maximum width in pixels or as a percentage.<br>Ex: 400px, 50%, etc.', 'theme-blvd-layout-builder' ),
+				'std'		=> '',
+				'type'		=> 'text'
+			),
 			'subgroup_start' => array(
 				'type'		=> 'subgroup_start',
 				'class'		=> 'show-hide-toggle'
